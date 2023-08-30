b4fd2cf80023359ceb28a8dce84bb073479c3639
GabrielBS46 <gabriel.bitencourt46@gmail.com>
2023-08-30 14:25:31 -0300

    Update geo.py

--- geo.py
+++ geo.py
@@ -1,11 +1,34 @@
-from geopy.geocoders import Nominatim
-import time
-from pprint import pprint
+import ipinfo
 import sys
+from geopy.geocoders import Nominatim
+from pprint import pprint
+from  socket import gethostbyname
 
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("Localizacao IP")
+print(ascii_banner)
+
+# Colocar Endereço na linha de comando
+try:
+    ip_address = sys.argv[1]
+except IndexError:
+    ip_address = None
+# Token do ipinfo.io
+access_token = 'e7021c81aa341c'
+handler = ipinfo.getHandler(access_token)
+details = handler.getDetails(gethostbyname(ip_address))
+
+# Conversão dos valores em strings
+for key, value in details.all.items():
+#    print(f"{key}: {value}")
+    if (key == 'ip'):
+        print("Endereço IP",value)
+    if (key == 'loc'):
+        geo = value
+
+### Geo Location IP - Usando coordenadas
 app = Nominatim(user_agent="tutorial")
-
-geo = sys.argv[1]
 
 location = app.geocode(geo).raw
 
@@ -15,5 +38,3 @@ city = location["display_name"]
 
 print(f"Segue coordenadas: {latitude},{longitude}")
 print(city)
-
-#print(location)
